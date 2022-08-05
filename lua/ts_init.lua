e729008817b49c3825e7621a8c334f101ce3bb76
Kellen Fox <kellen@cloudflare.com>
2022-08-04 18:41:58 -0700

    Init

---
+++ lua/ts_init.lua
@@ -0,0 +1,8 @@
+require'nvim-treesitter.configs'.setup {
+	ensure_installed = { "bash", "go", "gomod", "gowork" },
+	sync_install = true,
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+}
